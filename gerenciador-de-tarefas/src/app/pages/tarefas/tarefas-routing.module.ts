f7beb33938bc9fd8b7798159486f1863c1292512
ALEF BACELAR <74435383+Alefbacelar@users.noreply.github.com>
2022-12-21 17:29:00 -0300

    gerenciador de tarefas

---
+++ gerenciador-de-tarefas/src/app/pages/tarefas/tarefas-routing.module.ts
@@ -0,0 +1,26 @@
+
+import { Routes } from '@angular/router';
+import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
+import { ListarTarefaComponent } from './listar';
+
+export const TarefasRoutes: Routes = [
+  {
+		path: 'tarefas',
+		redirectTo: 'tarefas/listar'
+	},
+	{
+		path: 'tarefas/listar',
+		component: ListarTarefaComponent
+	},
+	{
+		path: 'tarefas/cadastrar',
+		component: CadastrarTarefaComponent
+	},
+	{
+		path: 'tarefas/editar/:id',
+		component: EditarTarefaComponent
+	}
+];
+
+
